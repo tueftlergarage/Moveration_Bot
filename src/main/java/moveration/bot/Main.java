f1d98d7f54947afd9c91ed15db6a593002165c60
tueftlergarage <thomasfischerebersgmail.com>
2021-08-16 18:17:04 +0200

    added Main methode

---
+++ src/main/java/moveration/bot/Main.java
@@ -0,0 +1,15 @@
+package moveration.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+
+        JDA jda = JDABuilder.createDefault("token").build();
+
+    }
+
+}
