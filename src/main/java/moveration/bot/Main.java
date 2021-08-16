d9642b5b3ff4dfdb5b1a28d177ee79e0b0f022d9
tueftlergarage <thomasfischerebersgmail.com>
2021-08-16 22:36:55 +0200

    added Helloworld command and first version of move Command

--- src/main/java/moveration/bot/Main.java
+++ src/main/java/moveration/bot/Main.java
@@ -1,13 +1,20 @@
 package moveration.bot;
 
-import net.dv8tion.jda.api.JDA;
+import lombok.val;
+import lombok.SneakyThrows;
+import moveration.bot.commands.HelloWorld;
+import moveration.bot.commands.MoveTestCommand;
+import moveration.bot.security.TokenReader;
 import net.dv8tion.jda.api.JDABuilder;
 
-import javax.security.auth.login.LoginException;
-
 public class Main {
-    public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault(TokenReader.getToken()).build();
-    }
 
+    @SneakyThrows
+    public static void main(String[] args) {
+        val jda = JDABuilder.createDefault(TokenReader.getToken()).build();
+        val eventManager = new EventManager();
+        eventManager.registerCommand(new HelloWorld());
+        eventManager.registerCommand(new MoveTestCommand());
+        jda.addEventListener(eventManager);
+    }
 }
