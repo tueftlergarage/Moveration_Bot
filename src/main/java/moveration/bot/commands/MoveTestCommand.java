17421157792928cf245b3a29e26cb722ed09f6c7
Jonas Mohr <jones.m.987654321@gmail.com>
2021-08-17 13:48:42 +0200

    - Added JDBC-Driver to connect to the SQLite Database to the pom.xml - Added simple Assert Utility-class - Added useless error-suppression

--- src/main/java/moveration/bot/commands/MoveTestCommand.java
+++ src/main/java/moveration/bot/commands/MoveTestCommand.java
@@ -1,5 +1,6 @@
 package moveration.bot.commands;
 
+import moveration.bot.util.Assert;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
@@ -22,6 +23,7 @@ public class MoveTestCommand implements Command {
         final Guild guild = e.getGuild();
         final Member member = e.getMember();
         final VoiceChannel target = guild.getVoiceChannelById(876914273476046918L);
+        Assert.allNotNull(guild, member, target);
         guild.moveVoiceMember(member, target).queue();
     }
 }
---
+++ src/main/java/moveration/bot/util/Assert.java
@@ -0,0 +1,17 @@
+package moveration.bot.util;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@UtilityClass
+public class Assert {
+
+    public static void allNotNull(@Nullable  Object... objects) {
+        if (Arrays.stream(objects).anyMatch(Objects::isNull))
+            throw new NullPointerException("Object was null");
+    }
+
+}
